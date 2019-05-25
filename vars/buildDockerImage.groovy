17d708d5e0aab68eafbe0bb7391bc71a7542c648
RekhaPrathap <50766560+RekhaPrathap@users.noreply.github.com>
2019-05-25 13:29:42 +0530

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -2,7 +2,7 @@ def call(String imageName){
 
 
 //connect to docker hub
- docker.withRegistry('https://registry.hub.docker.com', 'dockerimagepush') {
+ docker.withRegistry('https://registry.hub.docker.com', 'dockerhubpush') {
     
     //build docker image
     image = docker.build("${imageName}:${BUILD_NUMBER}")
