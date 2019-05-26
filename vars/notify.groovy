79b30e454adf6bd90250f5883c56727f72d587ac
RekhaPrathap <50766560+RekhaPrathap@users.noreply.github.com>
2019-05-26 12:49:45 +0530

    Create notify.groovy

---
+++ vars/notify.groovy
@@ -0,0 +1,8 @@
+def call(String status){
+emailext (
+      subject: "${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+      body: """<p>${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+        <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>""",
+      recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+    )
+}
